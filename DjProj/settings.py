8a22273eeeafc5341be5033807bb6a079c73a972
AB-Git23 <abdulbasitshaikh2323@gmail.com>
2025-08-06 17:06:55 +0500

    Added mysql & made a new venv with python v3.12..8 compatible with mysql
    Changed Database settings for mysql .
    Deleted the old venv based on python 3.13.5.

--- DjProj/settings.py
+++ DjProj/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'DjProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Mysql123Pass'
     }
 }
 
