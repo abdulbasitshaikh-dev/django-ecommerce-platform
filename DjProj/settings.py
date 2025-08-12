75596cf7539639cbf1c373c2e4c9ed61ae58fa5d
AB-Git23 <abdulbasitshaikh2323@gmail.com>
2025-08-12 17:30:41 +0500

    Enhance admin and models, add debug toolbar
    Added django-debug-toolbar to settings and URLs for development debugging. Improved admin interfaces for store and tags apps with custom filters, inlines, and display methods. Updated models with __str__ methods, ordering, and field validators for better data integrity and admin usability. Refactored hello.html template to include standard HTML structure.

--- DjProj/settings.py
+++ DjProj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'myapp',
     'store',
     'tags',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'DjProj.urls'
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Order, Product, Customer, Collection, OrderItem
+from tags.models import Tag, TaggedItem
+from django.db.models import Q, F
+from django.db.models.aggregates import Aggregate, Count, Max, Min, Sum, Avg
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 def home(request):
+    # queryset = Order.objects.select_related(
+    #     'customer').prefetch_related('orderitem_set').all().order_by('-placed_at')[:5]
+
+    # queryset = OrderItem.objects.filter(
+    #     product__id=1).aggregate(units_sold=Sum('quantity'))
+
+    # content_type = ContentType.objects.get_for_model(Product)
+
+    # queryset = TaggedItem.objects.select_related('tag').filter(
+    #     content_type = content_type,
+    #     object_id = 1
+    # )
+
+    # collection = Collection()
+    # collection.title = 'New Collection'
+    # collection.featured_product = Product(pk=1)
+    # collection.save()
+    
+
     return render(request, 'hello.html', {'name': 'Basit'})
