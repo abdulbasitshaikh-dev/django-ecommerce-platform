608643766f2cabf8918700249b9f25fd572c3b41
AB-Git23 <abdulbasitshaikh2323@gmail.com>
2025-08-21 21:46:36 +0500

    added nestedrouters from drf

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,14 @@
+from posixpath import basename
 from django.urls import path
 from . import views
 from rest_framework.routers import SimpleRouter, DefaultRouter
+from rest_framework_nested import routers
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + products_router.urls
