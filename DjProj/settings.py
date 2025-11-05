4484ff60711bc3bd56bab0d854adb045999f9a76
AB-Git23 <abdulbasitshaikh2323@gmail.com>
2025-11-05 14:24:07 +0500

    Configure SMTP email backend in settings
    Switched email backend from console to SMTP and added Gmail SMTP configuration settings for sending emails through the application.

--- DjProj/settings.py
+++ DjProj/settings.py
@@ -167,4 +167,9 @@ SIMPLE_JWT = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'abdulbasitshaikh2323@gmail.com'
+EMAIL_HOST_PASSWORD = 'sjjf hnna ktws rfdu'
